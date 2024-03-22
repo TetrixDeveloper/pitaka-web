2624c174dcd9dffd2dce99c26e940d963267dc23
Gio Ivan Maniego <maniego.gio11@gmail.com>
2024-03-22 18:16:05 +0800

    minor code alignment changes

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,14 +1,14 @@
 import { AppleIcon, GoogleIcon } from "./assets/icons";
 
 export const DownloadButtonData = [
-    {
-      icon: AppleIcon,
-      text: "Apple Store",
-      url: "#",
-    },
-    {
-      icon: GoogleIcon,
-      text: "Google Play",
-      url: "#",
-    },
-  ];
+  {
+    icon: AppleIcon,
+    text: "Apple Store",
+    url: "https://apps.apple.com/ph/app/pitaka-blockchain-wallet/id1644341925",
+  },
+  {
+    icon: GoogleIcon,
+    text: "Google Play",
+    url: "https://play.google.com/store/apps/details?id=com.pitakamobile",
+  },
+];
